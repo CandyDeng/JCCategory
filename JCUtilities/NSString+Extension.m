04c314d99f6dd541801d2fa72e95339dfac644e1
monda <monda@mondadeiMac-2.local>
2019-11-26 13:33:21 +0800

    删除全部文件

--- JCUtilities/NSString+Extension.m
+++ JCUtilities/NSString+Extension.m
@@ -9,39 +9,5 @@
 #import "NSString+Extension.h"
 
 @implementation NSString (Extension)
-+ (NSString *)unBlank:(NSString *)string{
-    return @"";
-}
-
-
-+ (NSString *)dataFromTimeInterval:(NSTimeInterval)timeInterval{
-    static NSDateFormatter *dateFormatter = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        dateFormatter = [[NSDateFormatter alloc] init];
-        dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"];
-        dateFormatter.dateFormat = @"yyyy-MM-dd";
-    });
-    return [dateFormatter stringFromDate:[NSDate dateWithTimeIntervalSince1970:timeInterval/1000]];
-}
-
-+ (NSString *)timeFromTimeInterval:(NSTimeInterval)timeInterval{
-    static NSDateFormatter *dateFormatter = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        dateFormatter = [[NSDateFormatter alloc] init];
-        dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"];
-        dateFormatter.dateFormat = @"yyyy-MM-dd hh:mm:ss";
-    });
-    return [dateFormatter stringFromDate:[NSDate dateWithTimeIntervalSince1970:timeInterval/1000]];
-}
-
-- (NSString*)doubleValueString{
-    return nil;
-}
-
-- (NSString*)marketUnitCount{
-    return nil;
-}
 
 @end
